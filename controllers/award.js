eef5bfdc16edf4e75d4b3b6f514f2ed6598e1c95
LordFeexz <anandafiqri1403@gmail.com>
2023-01-06 20:32:16 +0700

    query search

--- controllers/award.js
+++ controllers/award.js
@@ -1,9 +1,45 @@
+const { Op } = require("sequelize");
 const { Award } = require("../models/award");
 
 class Controller {
   static async getAllData(req, res, next) {
     try {
-      const data = await Award.findAll();
+      const { type, name, minPoin, maxPoin, pagination } = req.query;
+
+      let option = { where: {} };
+
+      option.limit = 6;
+      if (pagination) {
+        option.offset = 6 * pagination - 6;
+      } else {
+        option.offset = 6 * 1 - 6;
+      }
+
+      if (type) {
+        option.where.type = {
+          [Op.iLike]: `%${type}%`,
+        };
+      }
+
+      if (name) {
+        option.where.name = {
+          [Op.iLike]: `%${name}%`,
+        };
+      }
+
+      if (minPoin) {
+        option.where.poin = {
+          [Op.gte]: minPoin,
+        };
+      }
+
+      if (maxPoin) {
+        option.where.poin = {
+          [Op.lte]: maxPoin,
+        };
+      }
+
+      const data = await Award.findAll(option);
 
       if (data.length < 1) throw { name: "Data not found" };
 
