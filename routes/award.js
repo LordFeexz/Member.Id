1d9b0f5869d4a90f834ed4800780dba4b90970a5
LordFeexz <anandafiqri1403@gmail.com>
2023-01-06 18:19:08 +0700

    add authentication

--- routes/award.js
+++ routes/award.js
@@ -1,3 +1,10 @@
+const Controller = require("../controllers/award");
+const authentication = require("../middlewares/authentication");
+
 const router = require("express").Router();
+
+router.use(authentication);
+
+router.get("/", Controller.getAllData);
 
 module.exports = router;
